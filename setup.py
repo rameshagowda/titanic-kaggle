39ce18fc6323c7a48891dcd41eb6c3e0c0db3b5f
Ramesha Gowda <ramesha.gowda@gmail.com>
2018-05-03 07:52:45 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.0.1',
+    description='titanic data science challenge - Kaggle',
+    author='Your name (or your organization/company/team)',
+    license='MIT',
+)
